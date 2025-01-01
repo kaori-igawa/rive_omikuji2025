c75667259bfeb496eb6162828ccd9cf4949b2c85
kaori-igawa <kaori0v0igawa@gmail.com>
2025-01-01 16:02:02 +0900

    やっぱりviteだとriveのlayoutが使えない

--- src/components/animationView.tsx
+++ src/components/animationView.tsx
@@ -1,22 +1,16 @@
 import { useRive } from '@rive-app/react-canvas';
-// import { useRive, Layout, Fit, Alignment } from '@rive-app/react-canvas';
+
 
 import riveFile from '@data/omikuji2025.riv'
 
 const AnimationView = () => {
 
-  const riveParams = {
+  const { RiveComponent } = useRive({
     src: riveFile,
     artboard: 'Artboard',
     stateMachines: 'State Machine 1',
-    // layout: new Layout({
-    //   fit: Fit.Cover,
-    //   alignment: Alignment.TopCenter,
-    // }),
     autoplay: true,
-  };
-
-  const { RiveComponent } = useRive(riveParams);
+  });
 
   return <RiveComponent />;
 };
