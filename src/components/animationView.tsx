2e57929deb74faf26451b518b8563f5acf12ff0f
kaori-igawa <kaori0v0igawa@gmail.com>
2025-01-01 12:34:40 +0900

    rive実装

---
+++ src/components/animationView.tsx
@@ -0,0 +1,24 @@
+import { useRive } from '@rive-app/react-canvas';
+// import { useRive, Layout, Fit, Alignment } from '@rive-app/react-canvas';
+
+import riveFile from '@data/omikuji2025.riv'
+
+const AnimationView = () => {
+
+  const riveParams = {
+    src: riveFile,
+    artboard: 'Artboard',
+    stateMachines: 'State Machine 1',
+    // layout: new Layout({
+    //   fit: Fit.Cover,
+    //   alignment: Alignment.TopCenter,
+    // }),
+    autoplay: true,
+  };
+
+  const { RiveComponent } = useRive(riveParams);
+
+  return <RiveComponent />;
+};
+
+export default AnimationView;
