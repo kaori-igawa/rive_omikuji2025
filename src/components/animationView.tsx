19edcdbde33841cd053a50b745bcefa6ac2e0183
kaori-igawa <kaori0v0igawa@gmail.com>
2025-01-01 19:11:20 +0900

    loading画面の実装

--- src/components/animationView.tsx
+++ src/components/animationView.tsx
@@ -1,18 +1,28 @@
+import { useState } from 'react';
 import { useRive } from '@rive-app/react-canvas';
 
 
 import riveFile from '@data/omikuji2025.riv'
 
 const AnimationView = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const { RiveComponent } = useRive({
     src: riveFile,
     artboard: 'Artboard',
     stateMachines: 'State Machine 1',
     autoplay: true,
+    onLoad: () => {
+      setIsLoaded(true);
+    },
   });
 
-  return <RiveComponent />;
+  return (
+    <>
+      {!isLoaded && <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-2xl'>Loading...</div>}
+      <RiveComponent />
+    </>
+  );
 };
 
 export default AnimationView;
